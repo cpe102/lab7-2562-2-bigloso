2f23f8d4e0a15095bfc5f7aa3c309cbaccfb2094
Bigloso <wayside.big@gmail.com>
2020-01-21 16:13:09 +0700

    asdfghjkl

--- lab7_1.cpp
+++ lab7_1.cpp
@@ -9,15 +9,25 @@ int main()
   if (rank == 'S')
 {
   cout << "You have received Super Ultra Rare Unit!!!\n";
+  cout << "You have received 5 gems.\n";
+  cout << "You have received 1 gems.\n";
+  cout << "You have received 2000 coins.\n";
+  cout << "You have received very KAK items.\n";
 }
 else if(rank == 'A'){
   cout << "You have received 5 gems.\n";
+  cout << "You have received 1 gems.\n";
+  cout << "You have received 2000 coins.\n";
+  cout << "You have received very KAK items.\n";
 }
 else if(rank == 'B'){
   cout << "You have received 1 gems.\n";
+  cout << "You have received 2000 coins.\n";
+  cout << "You have received very KAK items.\n";
 }
 else if(rank == 'C'){
   cout << "You have received 2000 coins.\n";
+  cout << "You have received very KAK items.\n";
 }
 else if(rank == 'D'){
   cout << "You have received very KAK items.\n";
